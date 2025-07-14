ca18d39c226048990939196b42022b93e583701c
Patchuli <frostedproglide@proton.me>
2025-07-13 23:30:41 -0500

    chore: cleaned up hooks and added additional code attribution

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -2,45 +2,44 @@
 #include "include/I/InputManager.h"
 #include "include/M/Menu.h"
 
-static void hk_PollInputDevices(RE::BSTEventSource<RE::InputEvent*>* a_dispatcher, RE::InputEvent** a_events);
-static inline REL::Relocation<decltype(hk_PollInputDevices)> _InputHandler;  // local
-
-// Note to self: It's important that we read and process all events from the engine. When applying any sort of
-// conditional logic, we become suseptible to bugs caused by press/release events not being matched.
-
-void hk_PollInputDevices(RE::BSTEventSource<RE::InputEvent*>* a_dispatcher, RE::InputEvent** a_events)
-{
-	if (a_events) {
-		Modex::InputManager::GetSingleton()->AddEventToQueue(a_events);
-	}
-
-	if (Modex::Menu::IsEnabled()) {
-		static RE::InputEvent* dummy[] = { nullptr };
-		_InputHandler(a_dispatcher, dummy);
-	} else {
-		_InputHandler(a_dispatcher, a_events);
-	}
-}
+// Addresses for hooks, in large part, derived from Open Animation Replacer by Ersh
+// https://github.com/ersh1/OpenAnimationReplacer/blob/786b286ffc0f680d9cbe3e70f59c5cf2387d3017/src/Hooks.h
+// License: GNU General Public License v3.0 (WITH) Modding Exception
 
 namespace Hooks
 {
-	struct D3DInitHook
+	struct PollInputDevices_Hook
+	{
+		static void thunk(RE::BSTEventSource<RE::InputEvent*>* a_dispatcher, RE::InputEvent** a_events)
+		{
+			if (a_events) {
+				Modex::InputManager::GetSingleton()->AddEventToQueue(a_events);
+			}
+			
+			if (Modex::Menu::IsEnabled()) {
+				static RE::InputEvent* dummy[] = { nullptr };
+				func(a_dispatcher, dummy);
+			} else {
+				func(a_dispatcher, a_events);
+			}
+		}
+
+		static inline REL::Relocation<decltype(thunk)> func;
+	};
+	static inline REL::Relocation<uintptr_t> _input{ REL::VariantID(67315, 68617, 0xC519E0) };
+
+	struct D3D11Create_Hook
 	{
 		static void thunk()
 		{
 			func();
 
-			auto manager = RE::BSGraphics::Renderer::GetSingleton();
-			auto context = reinterpret_cast<ID3D11DeviceContext*>(manager->GetRuntimeData().context);
-			auto swapchain = reinterpret_cast<IDXGISwapChain*>(manager->GetRuntimeData().renderWindows->swapChain);
-			auto device = reinterpret_cast<ID3D11Device*>(manager->GetRuntimeData().forwarder);
-
-			Modex::Menu::GetSingleton()->Init(swapchain, device, context);
+			Modex::Menu::GetSingleton()->Init();
 		}
 
 		static inline REL::Relocation<decltype(thunk)> func;
-		static inline std::atomic<bool> init{ false };
 	};
+	static inline REL::Relocation<uintptr_t> _create{ REL::VariantID(75595, 77226, 0xDC5530) };
 
 	struct WndProcHandler_Hook
 	{
@@ -48,33 +47,34 @@ namespace Hooks
 		{
 			switch (a_msg) {
 			case WM_SETFOCUS:
-				Modex::InputManager::GetSingleton()->OnFocusChange(true);  // TODO: On Focus Change
+				Modex::InputManager::GetSingleton()->OnFocusChange(true);
 				break;
 			case WM_KILLFOCUS:
 				Modex::InputManager::GetSingleton()->OnFocusChange(false);
 				break;
 			}
 
-			// TODO: IME Implementation Here.
-
 			return func(a_hwnd, a_msg, a_wParam, a_lParam);
 		}
+
 		static inline REL::Relocation<decltype(thunk)> func;
 	};
-
+	
 	struct RegisterClassA_Hook
 	{
 		static ATOM thunk(WNDCLASSA* a_wndClass)
 		{
 			WndProcHandler_Hook::func = reinterpret_cast<uintptr_t>(a_wndClass->lpfnWndProc);
 			a_wndClass->lpfnWndProc = &WndProcHandler_Hook::thunk;
-
+			
 			return func(a_wndClass);
 		}
+		
 		static inline REL::Relocation<decltype(thunk)> func;
 	};
+	static inline REL::Relocation<uintptr_t> _register{ REL::VariantID(75591, 77226, 0xDC4B90) };
 
-	struct DXGIPresentHook
+	struct DXGIPresent_Hook
 	{
 		static void thunk(std::uint32_t a_p1)
 		{
@@ -82,24 +82,29 @@ namespace Hooks
 
 			Modex::Menu::GetSingleton()->Draw();
 		}
+
 		static inline REL::Relocation<decltype(thunk)> func;
 	};
-
+	static inline REL::Relocation<uintptr_t> _present{ REL::VariantID(75461, 77246, 0xDBBDD0) };
+	
 	void Install()
 	{
-		SKSE::AllocTrampoline(14);
 		auto& trampoline = SKSE::GetTrampoline();
-		logger::info("[Hook] Hooking BSInputDeviceManager::PollInputDevices");
-		_InputHandler = trampoline.write_call<5>(REL::RelocationID(67315, 68617).address() + REL::Relocate(0x7B, 0x7B, 0x81), hk_PollInputDevices);  //BSInputDeviceManager::PollInputDevices -> Inputfunc
 
-		logger::info("[Hook] Hooking BSGraphics::Renderer::InitD3D");
-		stl::write_thunk_call<D3DInitHook>(REL::RelocationID(75595, 77226).address() + REL::Relocate(0x50, 0x2BC));
-		D3DInitHook::init.store(true);
+		SKSE::AllocTrampoline(14);
+		PollInputDevices_Hook::func = trampoline.write_call<5>(_input.address() + REL::VariantOffset(0x7B, 0x7B, 0x81).offset(), PollInputDevices_Hook::thunk);
+		logger::debug("[Hook] Hooked PollInputDevices at {:#x}", _input.address() + REL::VariantOffset(0x7B, 0x7B, 0x81).offset());
 
-		logger::info("[Hook] Hooking WndProcHandler");
-		stl::write_thunk_call_6<RegisterClassA_Hook>(REL::VariantID(75591, 77226, 0xDC4B90).address() + REL::VariantOffset(0x8E, 0x15C, 0x99).offset());
+		SKSE::AllocTrampoline(8); // 14?
+		RegisterClassA_Hook::func = *(uintptr_t*)trampoline.write_call<6>(_register.address() + REL::VariantOffset(0x8E, 0x15C, 0x99).offset(), RegisterClassA_Hook::thunk);
+		logger::debug("[Hook] Hooked RegisterClassA at {:#x}", _register.address() + REL::VariantOffset(0x8E, 0x15C, 0x99).offset());
 
-		logger::info("[Hook] Hooking DXGI Present");
-		stl::write_thunk_call<DXGIPresentHook>(REL::RelocationID(75461, 77246).address() + REL::VariantOffset(0x9, 0x9, 0x15).offset());
+		SKSE::AllocTrampoline(14);
+		D3D11Create_Hook::func = trampoline.write_call<5>(_create.address() + REL::VariantOffset(0x9, 0x275, 0x9).offset(), D3D11Create_Hook::thunk);
+		logger::debug("[Hook] Hooked D3D11 Create at {:#x}", _create.address() + REL::VariantOffset(0x9, 0x275, 0x9).offset());
+
+		SKSE::AllocTrampoline(14);
+		DXGIPresent_Hook::func = trampoline.write_call<5>(_present.address() + REL::VariantOffset(0x9, 0x9, 0x15).offset(), DXGIPresent_Hook::thunk);
+		logger::debug("[Hook] Hooked DXGI Present at {:#x}", _present.address() + REL::VariantOffset(0x9, 0x9, 0x15).offset());
 	}
 }
