afd80d23403d742df299a77aec7d6343fb8aecd9
patchulidev <246538756+patchulidev@users.noreply.github.com>
2026-01-16 13:36:10 -0600

    added a window interface for code portability

---
+++ src/ui/components/UIWindow.h
@@ -0,0 +1,42 @@
+#pragma once
+
+namespace Modex
+{
+	class UIManager;  // Forward declaration
+
+	class UIWindow
+	{
+	public:
+		virtual ~UIWindow() = default;
+		virtual void Draw() = 0;
+        virtual void OnOpen() {}
+        virtual void OnClose() {}
+
+        virtual void OpenWindow(UIManager* a_manager);
+        virtual void CloseWindow();
+		virtual bool WantsInputCapture() const { return m_captureInput; }
+
+		virtual void Update(float a_deltaTime);
+
+		inline static constexpr float FADE_IN = 0.3f;
+		inline static constexpr float FADE_OUT = 0.3f;
+
+		UIManager* manager = nullptr;
+
+		
+	protected:
+		float m_alpha = 0.0f;
+		bool  m_close = false;
+		bool  m_captureInput = false;
+
+		enum class WindowState : uint8_t {
+			Opening,
+			Open,
+			Closing,
+			Closed
+		};
+
+		WindowState m_state = WindowState::Closed;
+
+	};
+}
